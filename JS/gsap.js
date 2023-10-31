1e01e1a93295450766f5677eb8592814d890bb6a
LuizGustavo0101 <111617474+LuizGustavo0101@users.noreply.github.com>
2023-10-31 20:09:23 -0300

    Add files via upload

--- JS/gsap.js
+++ JS/gsap.js
@@ -8,11 +8,11 @@ var resposta = document.querySelector(".resposta");
 // (biblioteca) - timeline usado para carregar um apos outro
 let tl = gsap.timeline()
 
-tl.to(".bola1", {y:-60, duration:1, repeat:-1, yoyo: true, ease: "easeOut"}, 0);
-tl.to(".bola2", {y:-60, duration:1, repeat:-1, delay:0.1, yoyo: true, ease: "easeOut"}, "<");
-tl.to(".bola3", {y:-60, duration:1, repeat:-1, delay:0.2, yoyo: true, ease: "easeOut"}, "<");
-tl.to(".bola4", {y:-60, duration:1, repeat:-1, delay:0.3, yoyo: true, ease: "easeOut"}, "<");
-tl.to(".bola5", {y:-60, duration:1, repeat:-1, delay:0.3, yoyo: true, ease: "easeOut"}, "<");
+tl.to(".bola1", {y:-40, duration:1, repeat:-1, yoyo: true, ease: "easeOut"}, 0);
+tl.to(".bola2", {y:-40, duration:1, repeat:-1, delay:0.1, yoyo: true, ease: "easeOut"}, "<");
+tl.to(".bola3", {y:-40, duration:1, repeat:-1, delay:0.2, yoyo: true, ease: "easeOut"}, "<");
+tl.to(".bola4", {y:-40, duration:1, repeat:-1, delay:0.3, yoyo: true, ease: "easeOut"}, "<");
+tl.to(".bola5", {y:-40, duration:1, repeat:-1, delay:0.3, yoyo: true, ease: "easeOut"}, "<");
 
 // carregar depois que todo o conteudo carregar
 // ou seja, o contador começará depois que tudo carregar
